57434e4c31c198c5ed4b93de0befc222f3f38156
Abishek-Balasubramaniam <65147392+Abishek-Balasubramaniam@users.noreply.github.com>
2020-07-15 20:07:40 +0530

    Add files via upload

--- sastra-notifier.py
+++ sastra-notifier.py
@@ -1,46 +1,39 @@
 from bs4 import BeautifulSoup
 import requests
 import time
-#from email.mime.text import MIMEText
-#import smtplib
 from notify_run import Notify
 new_circular=[]
 notify = Notify()
-x=notify.register()
-print(x)                
-#def Sastra_notifier(new_circular):
+#x=notify.register()
+#print(x)                
+
 while True:
     url = "https://www.sastra.edu/"
     
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     
-    response = requests.get(url, headers=headers)
+    response = requests.get(url,headers=headers)
     
     soup = BeautifulSoup(response.text, "lxml")
 
-    container = soup.find('div', class_='custom')
+    container = soup.find('div', id = 'user6')
 
-    rows = container.find_all('p')
+    rows = container.find_all('li')
+
     links = []
+    
     for row in rows:
-        if row.find('img'):
-            link = row.find('a', href=True)
-            links.append(link)
-    #print(links)
-        
-    if links == new_circular:
-        time.sleep(5)
-        #Sastra_notifier(new_circular)
+        link = row.find('a', href=True)
+        links.append(url[:-1]+link['href'])
 
-    else:
-        new_circular = links
-        count = len(links)
-        for link in links:
-            msg = str(count) + "new circular(s) from SASTRA : " + link.text            
-            notify.send(msg, link['href'])
-            print(link['href'])
-            time.sleep(5)
-            #Sastra_notifier(new_circular)
+    new_links=list(set(links)-set(new_circular))
+
+    for link in new_links:
+        msg = "New circular(s) from SASTRA : " + link            
+        notify.send(msg, link)
+        print(link)         
             
+    new_circular=links[:]
+    time.sleep(3)        
             
-#Sastra_notifier(new_circular)
+
